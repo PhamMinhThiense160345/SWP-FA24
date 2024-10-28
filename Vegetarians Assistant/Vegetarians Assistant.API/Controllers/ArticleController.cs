7f9595f8882f47c85ebba4bb71d0ceae5118dca9
PhamMinhThiense160345 <135082170+PhamMinhThiense160345@users.noreply.github.com>
2024-10-28 17:10:44 +0700

    byid

---
+++ Vegetarians Assistant/Vegetarians Assistant.API/Controllers/ArticleController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Vegetarians_Assistant.Repo.Repositories.Repo;
+using Vegetarians_Assistant.Services.ModelView;
+using Vegetarians_Assistant.Services.Services.Interface.ArticleImp;
+using Vegetarians_Assistant.Services.Services.Interface.IArticle;
+
+namespace Vegetarians_Assistant.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ArticleController : ControllerBase
+    {
+        private readonly IArticleService _articleService;
+        public ArticleController(IArticleService articleService) {
+            _articleService = articleService;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> getArticleDetail(int id)
+        {
+            try
+            {
+                var article = await _articleService.GetById(id);
+                return Ok(article);
+            }catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+
+
+    }
+}
